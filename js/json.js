ead47190da3a62f84734f29c559ebce9b33a296e
Favio <favio.ceroni17@gmail.com>
2021-11-10 13:15:25 -0300

    validacion,limpieza campos,registro

--- js/json.js
+++ js/json.js
@@ -1,18 +1,8 @@
-$(document).ready(function(){
-    //se ejecutara cuando la la pagina este cargada completamnete
+
+function dibujarTabla(persona){
+
     var tablapersona = $("#tablaPersonas");
 
-    var persona =[{
-        "nombre" :"juan",
-        "apellido":"urrutia",
-        "edad":24
-    },
-    {
-        "nombre" :"Ana",
-        "apellido":"Diaz",
-        "edad":20
-    }
-    ]
     var html4="<tr><th>Nombre</th><th>Apelldio</th><th>Edad</th></tr>";
     persona.forEach(function(item){
         html4+="<tr>";
@@ -28,4 +18,88 @@ $(document).ready(function(){
         html4+="</tr>"
     });
     tablapersona.html(html4);
-});
+}
+
+function limpiar(){
+    $("#txtNombre").val("");
+    $("#txtApellido").val("");
+    $("#txtEdad").val("");
+
+}
+
+$(document).ready(function(){
+    //se ejecutara cuando la la pagina este cargada completamnete
+
+    var persona =[{
+        "nombre" :"juan",
+        "apellido":"urrutia",
+        "edad":24
+    },
+    {
+        "nombre" :"Ana",
+        "apellido":"Diaz",
+        "edad":20
+    }
+    ]
+
+
+    var botonguardar =$("#btnGuardar");
+    botonguardar.click(function(){
+        var tomaNombre= $("#txtNombre").val();
+        var tomaApp= $("#txtApellido").val();
+        var tomaEdad= $("#txtEdad").val();
+        var listerrores =$("#listadoerrores");
+
+        var errores=[];
+
+        if (tomaNombre.length<3 || tomaNombre.length>15){
+            errores.push("ingrese un nombre correcto");
+        }else if(tomaNombre==""){
+            errores.push("el nombre no puede estar vacio");
+        }if (tomaApp==""){
+            errores.push("el apellido no puede estar vacio");
+        }else if (tomaApp.length<3 || tomaApp.length>15){
+            errores.push("ingrese un apellido correcto");
+        }if (tomaEdad==""){
+            errores.push("la edad no puede estar vacio");
+
+        }else if(isNaN(tomaEdad)){
+            errores.push("ingrese numeros");
+        
+        }if(errores.length>0){
+
+            var html5="";
+            errores.forEach(function(item){
+                html5+="<li>"+item+"</li>";
+            });
+            listerrores.html(html5);
+            return;
+        }
+
+        listerrores.html("");
+
+        var datos = {
+            "nombre":tomaNombre,
+            "apellido":tomaApp,
+            "edad":tomaEdad
+        }
+        persona.push(datos);
+        dibujarTabla(persona);
+        limpiar();
+        
+
+      
+        });
+        dibujarTabla(persona);
+
+
+        var btnLimpiar =$("#btnLimpiar");
+        btnLimpiar.click(function(){
+            limpiar();
+        });
+    });
+
+   
+
+
+   
